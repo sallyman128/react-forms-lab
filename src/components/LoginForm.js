3e96f8f2d2a0a465d9c9d22ca46c6c34174ffa9b
Salmaan <salmaan.arif.ali@gmail.com>
2021-08-20 14:03:19 -0400

    commit tree 7fb1afc261c5661fd7a01fc8639eea02ec9958fe

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,36 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: null,
+      password: null
+    };
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+       [e.target.name]: e.target.value
+      })
+  }
+
+  submitForm = (e) => {
+    e.preventDefault()
+    this.state.username && this.state.password ? this.props.handleLogin(this.state) : null;
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.submitForm}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleInputChange} value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleInputChange} value={this.state.password}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,23 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "",
+    };
+  }
+
+  handleOnChange = (e) => {
+    this.setState({
+      message: e.target.value
+    });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleOnChange} value={this.state.message}/>
+        <p style={{color: 'red'}}>{this.props.maxChars - this.state.message.length} characters remaining.</p>
       </div>
     );
   }
